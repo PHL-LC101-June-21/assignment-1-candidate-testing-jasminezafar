330d3635e3161849bf56415b6d2ab3e9ef917944
jasminezafar <85506485+jasminezafar@users.noreply.github.com>
2021-06-20 22:17:37 +0000

    added loop

--- candidate-testing.js
+++ candidate-testing.js
@@ -21,21 +21,16 @@ console.log("Hello "  + candidateName);
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-  let givenArray = [8, 'Orbit', 'Trajectory', 45];
-
-  let questions = ['Who was the first American woman in space?', 
-  'True or false: 5 kilometer == 5000 meters?', 
-  '(5 + 3)/2 * 10 = ?',
-  'Given the array + givenArray + what entry is at index 2', 
-  "What is the minimum crew size for the ISS?"];
+  let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?',"Given the array [8, 'orbit', 'trajectory' 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
    
-
   let correctAnswers = ["Sally Ride", 
   "true", 
   "40", 
   "Trajectory", 
   "3"];
 
+  for (let i = 0; i < questions.length; i++) {console.log(questions[i]);
+  }
 
   correctAnswer= "sally ride";
 candidateAnswer = input.question('Who was the first American women in space?');
@@ -49,7 +44,6 @@ if (candidateAnswer == correctAnswer) {
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
