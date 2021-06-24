a98cb8ff6a1e691e35c9e995ab71b893ab928607
jasminezafar <85506485+jasminezafar@users.noreply.github.com>
2021-06-24 16:47:01 +0000

    update variables

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,8 +7,8 @@ let candidateName = "";
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions = [];
-let correctAnswers = [];
+let questions = ['1) Who was the first American woman in space?', '2) True or false: 5000 kilometer == 5 kilometers?', '3) (5 + 3)/2 * 10 = ?',"4) Given the array [8, 'orbit', 'trajectory' 45], what entry is at index 2?", "5) What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 const input = require('readline-sync');
 let numberCorrect = 0;
@@ -23,11 +23,6 @@ console.log("Hello "  + candidateName);
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
- questions = ['1) Who was the first American woman in space?', '2) True or false: 5000 kilometer == 5 kilometers?', '3) (5 + 3)/2 * 10 = ?',"4) Given the array [8, 'orbit', 'trajectory' 45], what entry is at index 2?", "5) What is the minimum crew size for the ISS?"];
-   
- correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-
-
 
   for (let i = 0; i < questions.length; i++){
   candidateAnswers[i] = input.question(questions[i]); 
@@ -37,10 +32,7 @@ function askQuestion() {
 //console.log("Correct");
 numberCorrect = numberCorrect + 1;
   }
-
- 
-    //console.log("Incorrect!");
-  
+ //console.log("Incorrect!");  
   }
 
 }
