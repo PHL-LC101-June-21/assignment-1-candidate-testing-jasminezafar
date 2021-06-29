4b1cc40e67e60808a1c21d86ecfe588708478128
jasminezafar <85506485+jasminezafar@users.noreply.github.com>
2021-06-29 01:16:31 +0000

    updated to string

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,7 +6,7 @@ let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
-let candidateAnswer;
+let candidateAnswer = "";
 let questions = ['1) Who was the first American woman in space? ', '2) True or false: 5000 kilometer == 5 kilometers? ', '3) (5 + 3)/2 * 10 = ?',"4) Given the array [8, 'orbit', 'trajectory' 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
